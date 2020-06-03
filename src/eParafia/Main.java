9a4e68b1a0223daa1cbf84fd2d89e82a788b88e0
Piotrek <piotr-kaliciak@wp.pl>
2020-06-03 19:33:42 +0200

    Zmieniony tytuł

--- src/eParafia/Main.java
+++ src/eParafia/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("FXML/login.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("eParafia");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
