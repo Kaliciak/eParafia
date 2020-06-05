3de2d7a7fcec4b166cd478e021cd19b3cc544ea0
Kamil Galewski <kamgal.pl@gmail.com>
2020-06-05 16:48:27 +0200

    Parafianie selector created

---
+++ src/eParafia/Controller/FullParafianieEntity.java
@@ -0,0 +1,230 @@
+package eParafia.Controller;
+
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import static eParafia.Controller.Dane.connection;
+
+public class FullParafianieEntity {
+    SimpleIntegerProperty id_osoby;
+    SimpleIntegerProperty id_parafii;
+    SimpleStringProperty imie;
+    SimpleStringProperty drugie_imie;
+    SimpleStringProperty imie_z_bierzmowania;
+    SimpleStringProperty nazwisko;
+    SimpleStringProperty plec;
+    SimpleIntegerProperty id_adresu;
+    Date data_narodzin;
+    Date data_zgonu;
+    SimpleIntegerProperty id_ojca;
+    SimpleIntegerProperty id_matki;
+    SimpleIntegerProperty id_ojca_chrzestnego;
+    SimpleIntegerProperty id_matki_chrzestnej;
+    public FullParafianieEntity(int id_osoby, int id_parafii, String imie, String drugie_imie, String imie_z_bierzmowania,String nazwisko, String plec, int id_adresu, Date data_narodzin, Date data_zgonu, int id_ojca, int id_matki, int id_ojca_chrzestnego, int id_matki_chrzestnej) {
+        this.id_osoby=new SimpleIntegerProperty(id_osoby);
+        this.id_parafii = new SimpleIntegerProperty(id_parafii);
+        this.imie = new SimpleStringProperty(imie);
+        this.drugie_imie=new SimpleStringProperty(drugie_imie);
+        this.imie_z_bierzmowania=new SimpleStringProperty(imie_z_bierzmowania);
+        this.nazwisko = new SimpleStringProperty(nazwisko);
+        this.plec=new SimpleStringProperty(plec);
+        this.id_adresu=new SimpleIntegerProperty(id_adresu);
+        this.data_narodzin=data_narodzin;
+        this.data_zgonu=data_zgonu;
+        this.id_ojca=new SimpleIntegerProperty(id_ojca);
+        this.id_matki=new SimpleIntegerProperty(id_matki);
+        this.id_ojca_chrzestnego = new SimpleIntegerProperty(id_ojca_chrzestnego);
+        this.id_matki_chrzestnej =new SimpleIntegerProperty(id_matki_chrzestnej);
+    }
+    static public ArrayList<FullParafianieEntity> takeAllFromQuery(String query){
+        ArrayList<FullParafianieEntity> out = new ArrayList<>();
+        try {
+            Statement stmt = connection.createStatement();
+            ResultSet rs=stmt.executeQuery(query);
+            while(rs.next())
+                out.add(new FullParafianieEntity(
+                    rs.getInt("id_osoby"),
+                    rs.getInt("id_parafii"),
+                    rs.getString("imie"),
+                    rs.getString("drugie_imie"),
+                    rs.getString( "imie_z_bierzmowania"),
+                    rs.getString("nazwisko"),
+                    rs.getString("plec"),
+                    rs.getInt("id_adresu"),
+                    rs.getDate("data_narodzin"),
+                    rs.getDate("data_zgonu"),
+                    rs.getInt("id_ojca"),
+                    rs.getInt("id_matki"),
+                    rs.getInt("id_ojca_chrzestnego"),
+                    rs.getInt("id_matki_chrzestnej")
+                ));
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return out;
+    }
+    public int getId_osoby() {
+        return id_osoby.get();
+    }
+
+    public SimpleIntegerProperty id_osobyProperty() {
+        return id_osoby;
+    }
+
+    public void setId_osoby(int id_osoby) {
+        this.id_osoby.set(id_osoby);
+    }
+
+    public int getId_parafii() {
+        return id_parafii.get();
+    }
+
+    public SimpleIntegerProperty id_parafiiProperty() {
+        return id_parafii;
+    }
+
+    public void setId_parafii(int id_parafii) {
+        this.id_parafii.set(id_parafii);
+    }
+
+    public String getImie() {
+        return imie.get();
+    }
+
+    public SimpleStringProperty imieProperty() {
+        return imie;
+    }
+
+    public void setImie(String imie) {
+        this.imie.set(imie);
+    }
+
+    public String getDrugie_imie() {
+        return drugie_imie.get();
+    }
+
+    public SimpleStringProperty drugie_imieProperty() {
+        return drugie_imie;
+    }
+
+    public void setDrugie_imie(String drugie_imie) {
+        this.drugie_imie.set(drugie_imie);
+    }
+
+    public String getImie_z_bierzmowania() {
+        return imie_z_bierzmowania.get();
+    }
+
+    public SimpleStringProperty imie_z_bierzmowaniaProperty() {
+        return imie_z_bierzmowania;
+    }
+
+    public void setImie_z_bierzmowania(String imie_z_bierzmowania) {
+        this.imie_z_bierzmowania.set(imie_z_bierzmowania);
+    }
+
+    public String getNazwisko() {
+        return nazwisko.get();
+    }
+
+    public SimpleStringProperty nazwiskoProperty() {
+        return nazwisko;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko.set(nazwisko);
+    }
+
+    public String getPlec() {
+        return plec.get();
+    }
+
+    public SimpleStringProperty plecProperty() {
+        return plec;
+    }
+
+    public void setPlec(String plec) {
+        this.plec.set(plec);
+    }
+
+    public int getId_adresu() {
+        return id_adresu.get();
+    }
+
+    public SimpleIntegerProperty id_adresuProperty() {
+        return id_adresu;
+    }
+
+    public void setId_adresu(int id_adresu) {
+        this.id_adresu.set(id_adresu);
+    }
+
+    public Date getData_narodzin() {
+        return data_narodzin;
+    }
+
+    public void setData_narodzin(Date data_narodzin) {
+        this.data_narodzin = data_narodzin;
+    }
+
+    public Date getData_zgonu() {
+        return data_zgonu;
+    }
+
+    public void setData_zgonu(Date data_zgonu) {
+        this.data_zgonu = data_zgonu;
+    }
+
+    public int getId_ojca() {
+        return id_ojca.get();
+    }
+
+    public SimpleIntegerProperty id_ojcaProperty() {
+        return id_ojca;
+    }
+
+    public void setId_ojca(int id_ojca) {
+        this.id_ojca.set(id_ojca);
+    }
+
+    public int getId_matki() {
+        return id_matki.get();
+    }
+
+    public SimpleIntegerProperty id_matkiProperty() {
+        return id_matki;
+    }
+
+    public void setId_matki(int id_matki) {
+        this.id_matki.set(id_matki);
+    }
+
+    public int getId_ojca_chrzestnego() {
+        return id_ojca_chrzestnego.get();
+    }
+
+    public SimpleIntegerProperty id_ojca_chrzestnegoProperty() {
+        return id_ojca_chrzestnego;
+    }
+
+    public void setId_ojca_chrzestnego(int id_ojca_chrzestnego) {
+        this.id_ojca_chrzestnego.set(id_ojca_chrzestnego);
+    }
+
+    public int getId_matki_chrzestnej() {
+        return id_matki_chrzestnej.get();
+    }
+
+    public SimpleIntegerProperty id_matki_chrzestnejProperty() {
+        return id_matki_chrzestnej;
+    }
+
+    public void setId_matki_chrzestnej(int id_matki_chrzestnej) {
+        this.id_matki_chrzestnej.set(id_matki_chrzestnej);
+    }
+}
--- src/eParafia/Controller/SakramentView.java
+++ src/eParafia/Controller/SakramentView.java
@@ -1,12 +1,23 @@
 package eParafia.Controller;
 
+import eParafia.Main;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 import javax.xml.crypto.Data;
 
@@ -52,21 +63,83 @@ public class SakramentView {
     @FXML
     private Text parafiaName;
 
-    @FXML
-    private TableView<?> participants;
+    public class SakramentParticipant {
+        SimpleIntegerProperty id;
+        SimpleStringProperty imie;
+        SimpleStringProperty nazwisko;
+        SimpleStringProperty typ_uczestnictwa;
+        public SakramentParticipant(int id, String imie, String nazwisko, String typ_uczestnictwa) {
+            this.id=new SimpleIntegerProperty(id);
+            this.imie = new SimpleStringProperty(imie);
+            this.nazwisko=new SimpleStringProperty(nazwisko);
+            this.typ_uczestnictwa=new SimpleStringProperty(typ_uczestnictwa);
+        }
+
+        public int getId() {
+            return id.get();
+        }
+
+        public SimpleIntegerProperty idProperty() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id.set(id);
+        }
+
+        public String getImie() {
+            return imie.get();
+        }
+
+        public SimpleStringProperty imieProperty() {
+            return imie;
+        }
+
+        public void setImie(String imie) {
+            this.imie.set(imie);
+        }
+
+        public String getNazwisko() {
+            return nazwisko.get();
+        }
+
+        public SimpleStringProperty nazwiskoProperty() {
+            return nazwisko;
+        }
+
+        public void setNazwisko(String nazwisko) {
+            this.nazwisko.set(nazwisko);
+        }
+
+        public String getTyp_uczestnictwa() {
+            return typ_uczestnictwa.get();
+        }
+
+        public SimpleStringProperty typ_uczestnictwaProperty() {
+            return typ_uczestnictwa;
+        }
+
+        public void setTyp_uczestnictwa(String typ_uczestnictwa) {
+            this.typ_uczestnictwa.set(typ_uczestnictwa);
+        }
+    }
 
     @FXML
-    private TableColumn<?, ?> idColumn;
+    private TableView<SakramentParticipant> participants;
 
     @FXML
-    private TableColumn<?, ?> imieColumn;
+    private TableColumn<SakramentParticipant, Integer> idColumn;
 
     @FXML
-    private TableColumn<?, ?> nazwiskoColumn;
+    private TableColumn<SakramentParticipant, String> imieColumn;
 
     @FXML
-    private TableColumn<?, ?> uczestnictwoColumn;
+    private TableColumn<SakramentParticipant, String> nazwiskoColumn;
 
+    @FXML
+    private TableColumn<SakramentParticipant, String> uczestnictwoColumn;
+
+    ObservableList<SakramentParticipant> sakramentPersons = FXCollections.observableArrayList();
     @FXML
     private Text szafarzName;
 
@@ -74,10 +147,23 @@ public class SakramentView {
     private Text szafarzSurname;
 
     @FXML
-    void add(MouseEvent event) {
+    void add(ActionEvent event) {
+        ChooseParafianie.selectParafianie("select * from parafianie").stream().forEach(pr ->System.out.println(pr.getImie()));
+    }
+    @FXML
+    void add_gosc(ActionEvent event) {
 
     }
 
+    @FXML
+    void add_swiadek(ActionEvent event) {
+
+    }
+
+    @FXML
+    void delete(ActionEvent event) {
+
+    }
     @FXML
     void edit(ActionEvent event) {
 
@@ -109,6 +195,26 @@ public class SakramentView {
             catch (Exception e) {
                 e.printStackTrace();
             }
+            try {
+                Statement stmt = connection.createStatement();
+                String query = "select id_osoby, imie, nazwisko, typ_uczestnictwa from sakramenty_osoby left outer join parafianie using(id_osoby) where id_sakramentu="+currentSakrament.getId();
+                ResultSet rs = stmt.executeQuery(query);
+                while(rs.next()) {
+                    sakramentPersons.add(new SakramentParticipant(
+                            rs.getInt("id_osoby"),
+                            rs.getString("imie"),
+                            rs.getString("nazwisko"),
+                            rs.getString("typ_uczestnictwa")
+                     ));
+                }
+                idColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
+                imieColumn.setCellValueFactory(new PropertyValueFactory<>("imie"));
+                nazwiskoColumn.setCellValueFactory(new PropertyValueFactory<>("nazwisko"));
+                uczestnictwoColumn.setCellValueFactory(new PropertyValueFactory<>("typ_uczestnictwa"));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            participants.setItems(sakramentPersons);
         }
     }
 }
